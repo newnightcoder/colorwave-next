b1a008f1736d23ae6286a07d5d2eacec79e79f20
Nightcoder <newnightcoder@gmail.com>
2022-02-27 22:10:47 +0100

    redux-persist ssr error fixed with createWebStorage/createNoopStorage :rocket:

--- store/storeConfig.js
+++ store/storeConfig.js
@@ -1,9 +1,30 @@
 import { applyMiddleware, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/root.reducer.js";
+
+// creation of dummy storage on the server-side (we don't want to share persisted state with multiple clients) + wrapper for locale storage on the client-side
+// https://github.com/vercel/next.js/discussions/15687
+const createNoopStorage = () => {
+  return {
+    getItem(_key) {
+      return Promise.resolve(null);
+    },
+    setItem(_key, value) {
+      return Promise.resolve(value);
+    },
+    removeItem(_key) {
+      return Promise.resolve();
+    },
+  };
+};
+
+const storage =
+  typeof window !== "undefined"
+    ? createWebStorage("local")
+    : createNoopStorage();
 
 const persistConfig = {
   key: "root",
